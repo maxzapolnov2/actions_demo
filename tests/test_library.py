305a9bdd0ba569bd1dd428b38f7017b95fc47584
maxzapolnov2 <maxzapolnov2@gmail.com>
2025-11-26 19:19:27 +0300

    Update test_library.py

--- tests/test_library.py
+++ tests/test_library.py
@@ -2,7 +2,8 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+# Добавляем путь к вашей библиотеке
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 
 from actions_demo.library import Book, PrintedBook, EBook, User, Librarian, Library
 
