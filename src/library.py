84086f97a71feeaff55239368dedc6f8d8f6f300
maxzapolnov2 <maxzapolnov2@gmail.com>
2025-12-02 14:19:55 +0300

    Update library.py

--- src/library.py
+++ src/library.py
@@ -1,27 +1,8 @@
-"""Library management system"""
+"""Library system"""
 
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title):
         self.title = title
-        self.author = author
-        self.available = True
     
     def get_title(self):
         return self.title
-    
-    def is_available(self):
-        return self.available
-
-
-class Library:
-    def __init__(self):
-        self.books = []
-    
-    def add_book(self, book):
-        self.books.append(book)
-    
-    def find_book(self, title):
-        for book in self.books:
-            if book.get_title() == title:
-                return book
-        return None
